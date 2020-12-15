e22bfa27bac3098689a5b48e896db0a1c4fbabdd
guillempla <guillem.pla@est.fib.upc.edu>
2020-12-15 18:21:25 +0100

    Init activation_functions class

---
+++ src/activation_functions.cc
@@ -0,0 +1,9 @@
+#include "activation_functions"
+
+//___________CONSTRUCTORS__________
+Activation_functions::Activation_functions();
+
+//___________SETTERS__________
+
+
+//___________GETTERS__________
---
+++ src/activation_functions.hh
@@ -0,0 +1,24 @@
+#ifndef ACTIVATION_FUNCTIONS_HH
+#define ACTIVATION_FUNCTIONS_HH
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Activation_functions {
+    private:
+
+
+    public:
+        //___________CONSTRUCTORS__________
+        Activation_functions();
+
+        //___________SETTERS__________
+
+
+        //___________GETTERS__________
+
+}
+
+#endif
