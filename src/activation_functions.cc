e21c703bd226dd6cfd3eca7b6b7d129c9bc7f0e7
guillempla <guillem.pla@est.fib.upc.edu>
2020-12-15 20:45:46 +0100

    Added activation functions and its derivatives

--- src/activation_functions.cc
+++ src/activation_functions.cc
@@ -1,9 +1,40 @@
 #include "activation_functions"
 
 //___________CONSTRUCTORS__________
-Activation_functions::Activation_functions();
+Activation_functions::Activation_functions(const float steepness) {
+    this->steepness = steepness;
+}
 
 //___________SETTERS__________
+void Activation_functions::modify_steepness(const float steepness) {
+    this->steepness = steepness;
+}
 
 
 //___________GETTERS__________
+float Activation_functions::get_steepness() const {
+    return this->steepness;
+}
+
+
+// Activation functions
+float Activation_functions::sigmoid(const float z) const {
+    return 1.0 / (1 + exp(-z*this->steepness));
+}
+
+float Activation_functions::relu(const float z) const {
+    return max(z, 0);
+}
+
+
+// Dervatives of activation functions
+float Activation_functions::sigmoid_prime(const float z) const {
+    return this->sigmoid(z) / (1 - this->sigmoid(z*this->steepness));
+}
+
+float Activation_functions::relu_prime(const float z) const {
+    if (z > 0)
+        return 1;
+    else if (z < 0)
+        return 0;
+}
--- src/activation_functions.hh
+++ src/activation_functions.hh
@@ -3,21 +3,34 @@
 
 #include <iostream>
 #include <vector>
+#include <math.h>
 
 using namespace std;
 
 class Activation_functions {
     private:
-
+        float steepness;
 
     public:
         //___________CONSTRUCTORS__________
-        Activation_functions();
+        Activation_functions(const float steepness);
 
         //___________SETTERS__________
+        void modify_steepness(const float steepness);
 
 
         //___________GETTERS__________
+
+        // Return steepness private parameter
+        float steepness() const;
+
+        // Activation functions
+        float sigmoid(const float z) const;
+        float relu(const float z) const;
+
+        // Dervatives of activation functions
+        float sigmoid_prime(const float z) const;
+        float relu_prime(const float z) const;
 
 }
 
