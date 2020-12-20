7e0d33e8124f13a0703edaf6368fea426cebcaa8
guillempla <guillem.pla@est.fib.upc.edu>
2020-12-20 11:43:08 +0100

    Initialized matrix

---
+++ src/matrix.cc
@@ -0,0 +1,22 @@
+#include "matrix.hh"
+
+//___________CONSTRUCTORS__________
+Matrix::Matrix() {}
+
+
+//___________SETTERS__________
+
+
+//___________GETTERS__________
+Matrix Matrix::matsum(const Matrix& m1, const Matrix& m2) const {}
+
+Matrix Matrix::matsub(const Matrix& m1, const Matrix& m2) const {}
+
+Matrix Matrix::matdiv(const Matrix& m1, const Matrix& m2) const {}
+
+Matrix Matrix::matmul(const Matrix& m1, const Matrix& m2) const {}
+
+Matrix Matrix::matdot(const Matrix& m1, const Matrix& m2) const {}
+
+
+static void Matrix::print_matrix(const Matrix& m) {}
---
+++ src/matrix.hh
@@ -0,0 +1,35 @@
+#ifndef MATRIX_HH
+#define MATRIX_HH
+
+#include <iostream>
+#include <vector>
+#include <math.h>
+
+using namespace std;
+
+class Matrix {
+    private:
+        int m, n;
+        float data;
+
+    public:
+        //___________CONSTRUCTORS__________
+        Matrix(int m, int n);
+
+        //___________SETTERS__________
+
+
+        //___________GETTERS__________
+        Matrix matsum(const Matrix& m1, const Matrix& m2) const;
+        Matrix matsub(const Matrix& m1, const Matrix& m2) const;
+        Matrix matdiv(const Matrix& m1, const Matrix& m2) const;
+        Matrix matmul(const Matrix& m1, const Matrix& m2) const;
+        Matrix matdot(const Matrix& m1, const Matrix& m2) const;
+
+        static void print_matrix(const Matrix& m);
+
+
+
+}
+
+#endif
