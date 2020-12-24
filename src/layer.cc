654b0ae9b561d591f626dadc68673af1546cba10
guillempla <guillem.pla@est.fib.upc.edu>
2020-12-24 12:28:02 +0100

    Added getters and setters

--- src/layer.cc
+++ src/layer.cc
@@ -1,9 +1,49 @@
 #include "layer.hh"
 
 //___________CONSTRUCTORS__________
-Layer::Layer() {}
+Layer::Layer(int type, int layer_size, int next_size) {
+    this->type;
+    this->layer_size;
+    this->next_size;
+
+    this->initialize_bias();
+    this->initialize_weights();
+}
 
 //___________SETTERS__________
+void Layer::set_type(int type) {
+    this->type = type;
+}
+
+void Layer::set_bias(const Matrix& bias) {
+    this->bias = bias;
+}
+
+void Layer::set_weights(const Matrix& weights) {
+    this->weights = weights;
+}
+
 
 
 //___________GETTERS__________
+int Layer::get_type() const {
+    return this->type;
+}
+
+Matrix Layer::get_bias() const {
+    return this->bias;
+}
+
+Matrix Layer::get_weights() const {
+    return this->weights;
+}
+
+
+//___________PRIVATE__________
+void Layer::initialize_bias() {
+
+}
+
+void Layer::initialize_weights() {
+
+}
--- src/layer.hh
+++ src/layer.hh
@@ -4,22 +4,40 @@
 #include <iostream>
 #include <vector>
 #include <math.h>
+#include "matrix.hh"
 
 using namespace std;
 
 class Layer {
     private:
+        int type;       // 0: input layer; 1: hidden layer, 2: output layer
+        int layer_size;
+        int next_size;
+
+        Matrix bias;    // matrix of (next_size,1)
+        Matrix weights; // matrix of (next_size,actual_size)
+
+        void initialize_bias();
+        void initialize_weights();
 
     public:
         //___________CONSTRUCTORS__________
-        Layer();
+        Layer(int type, int layer_size, int next_size);
 
         //___________SETTERS__________
+        void set_type(int type);
+        void set_layer_size(int layer_size);
+        void set_next_size(int next_size);
+        void set_bias(const Matrix& bias);
+        void set_weights(const Matrix& weights);
 
 
         //___________GETTERS__________
+        int get_type() const;
+        Matrix get_bias() const;
+        Matrix get_weights() const;
 
-        
+
 
 };
 
