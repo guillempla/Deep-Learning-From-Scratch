0de6908bee6a6fdd78eabbfe8588e3d90a77fcc6
guillempla <guillem.pla@est.fib.upc.edu>
2020-12-24 11:32:42 +0100

    Initialize model class

---
+++ src/model.cc
@@ -0,0 +1,9 @@
+#include "model.hh"
+
+//___________CONSTRUCTORS__________
+Model::Model() {}
+
+//___________SETTERS__________
+
+
+//___________GETTERS__________
---
+++ src/model.hh
@@ -0,0 +1,26 @@
+#ifndef MODEL_HH
+#define MODEL_HH
+
+#include <iostream>
+#include <vector>
+#include <math.h>
+
+using namespace std;
+
+class Model {
+    private:
+
+    public:
+        //___________CONSTRUCTORS__________
+        Model();
+
+        //___________SETTERS__________
+
+
+        //___________GETTERS__________
+
+        
+
+};
+
+#endif
