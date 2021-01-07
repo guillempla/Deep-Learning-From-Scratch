93c0061897ae567e983328b02a92b81f86dc4b01
guillempla <guillem.pla@est.fib.upc.edu>
2021-01-07 23:08:53 +0100

    Renamed activation functions

---
+++ src/activation.cc
@@ -0,0 +1,44 @@
+#include "activation.hh"
+
+//___________CONSTRUCTORS__________
+Activation::Activation() {
+    this->steepness = 1;
+}
+
+Activation::Activation(double steepness) {
+    this->steepness = steepness;
+}
+
+//___________SETTERS__________
+void Activation::modify_steepness(double steepness) {
+    this->steepness = steepness;
+}
+
+
+//___________GETTERS__________
+double Activation::get_steepness() const {
+    return this->steepness;
+}
+
+
+// Activation functions
+double Activation::sigmoid(const double z) {
+    return 1.0 / (1 + exp(-z));
+}
+
+double Activation::relu(const double z) {
+    return max(z, 0.0);
+}
+
+
+// Dervatives of activation functions
+double Activation::sigmoid_prime(const double z) {
+    return Activation::sigmoid(z) / (1 - Activation::sigmoid(z));
+}
+
+double Activation::relu_prime(const double z) {
+    if (z > 0)
+        return 1;
+    else if (z < 0)
+        return 0;
+}
