3a3637a76f9f17377bdfe7d02d961819f2a32377
guillempla <guillem.pla@est.fib.upc.edu>
2021-01-11 16:33:16 +0100

    Fixed Layer initialization

--- src/layer.cc
+++ src/layer.cc
@@ -2,14 +2,10 @@
 
 //___________CONSTRUCTORS__________
 Layer::Layer(string type, unsigned num_examples, unsigned layer_size, unsigned prev_size) {
-    cout << "        Layer::Initializing layer" << endl;
-    this->type;
-    cout << "        Layer::Initialized type: " << type << endl;
-    this->layer_size;
-    this->prev_size;
-    this->num_examples;
-
-    cout << "        Layer::Finished init atributes" << endl;
+    this->type = type;
+    this->num_examples = num_examples;
+    this->layer_size = layer_size;
+    this->prev_size = prev_size;
 
     unsigned seed = 1;
 
@@ -65,36 +61,43 @@ void Layer::set_activation_gradient(Matrix& dA) {
 
 Matrix* Layer::feed_forward(Matrix& A_prev) {
     cout << "    Layer::feed_forward" << endl;
-    cout << "    Layer::Type: " << type << endl;
+    cout << "    Layer::Type: " << this->type << endl;
     cout << "    Layer::A_prev dimensions(" << A_prev.getRows() << "," << A_prev.getCols() << ")" << endl;
     cout << "    Layer::W dimensions(" << W.getRows() << "," << W.getCols() << ")" << endl;
     cout << "    Layer::b dimensions(" << b.getRows() << "," << b.getCols() << ")" << endl;
     Z = W*A_prev + b;
-    cout << "    Layer::Z calculated" << endl;
     cout << "    Layer::Z dimensions(" << Z.getRows() << "," << Z.getCols() << ")" << endl;
     if (this->type == "output")
         A = Z.sigmoid();
     else
         A = Z.relu();
-    cout << "    Layer::A calculated" << endl;
+    cout << "    Layer::A dimensions(" << A.getRows() << "," << A.getCols() << ")" << endl;
     return &A;
 }
 
 Matrix Layer::back_propagate(Matrix& A_prev) {
+    cout << "    Layer::Initialized back_propagate" << endl;
     if (type == "output") {
+        cout << "    Layer::Type Output" << endl;
         Matrix g_prime = Z.sigmoid_prime();
         dZ = dA*g_prime;
     }
     else {
+        cout << "    Layer::Type Hidden" << endl;
         Matrix g_prime = Z.relu_prime();
         dZ = dA*g_prime;
     }
+    cout << "    Layer::Calculated dZ" << endl;
 
     unsigned m = A_prev.getCols();
     Matrix A_prevT = A_prev.transpose();
+    cout << "    Layer::Transposed A_prev" << endl;
     dW = (dZ*A_prevT)/m;
+    cout << "    Layer::Calculated dW" << endl;
     db = (dZ.sum(1)/m).transpose();
+    cout << "    Layer::Calculated db" << endl;
     Matrix dA_prev = W.transpose()*dZ;
+    cout << "    Layer::Calculated dA_prev" << endl;
     return dA_prev;
 }
 
--- src/layer.hh
+++ src/layer.hh
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <vector>
 #include <math.h>
+#include <string>
 #include "matrix.hh"
 
 using namespace std;
