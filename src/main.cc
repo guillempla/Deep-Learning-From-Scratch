59c35be9c15bce006cb0513eff278f757edb64ef
guillempla <guillem.pla@est.fib.upc.edu>
2021-01-15 09:30:57 +0100

    Updated main

--- src/main.cc
+++ src/main.cc
@@ -36,18 +36,18 @@ void test() {
 }
 
 void real_main() {
-    cout << "Started main" << endl;
+    cout << "Started ./network" << endl;
 
     Data_processing d;
     Matrix y_test = d.read_test_labels();
     Matrix x_test = d.read_test_vectors();
     Matrix y_train = d.read_train_labels();
     Matrix x_train = d.read_train_vectors();
-    cout << "Finished read data" << endl;
+    cout << "Finished reading data" << endl;
 
-    vector<unsigned> layers_dims{ x_train.getRows(), 10, 10 };
-    vector<string> layers_type{ "relu", "sigmoid" };
-    Model m(x_train, y_train, layers_dims, layers_type, 0.0075, 10);
+    vector<unsigned> layers_dims{ x_train.getRows(), 256, 64, 10 };
+    vector<string> layers_type{ "relu", "relu", "softmax" };
+    Model m(x_train, y_train, "cross_entropy", layers_dims, layers_type, 0.001, 20);
     Matrix costs = m.train();
     cout << "Finished training" << endl;
 
