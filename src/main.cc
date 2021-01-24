d3d6359436212a3d128cf4eb96883e250264fc44
guillempla <guillem.pla@est.fib.upc.edu>
2021-01-24 23:10:20 +0100

    Definitive main

--- src/main.cc
+++ src/main.cc
@@ -36,7 +36,7 @@ void real_main() {
 
     vector<unsigned> layers_dims{ x_train.getRows(), 256, 64, 10 };
     vector<string> layers_type{ "relu", "relu", "sigmoid" };
-    Model m(x_train, y_train, "binary_cross_entropy", layers_dims, layers_type, 0.001, 60, 64, 0.7);
+    Model m(x_train, y_train, "binary_cross_entropy", layers_dims, layers_type, 0.001, 35, 32, 0.7);
     cout << "Finished initalizing" << endl;
 
     m.train();
